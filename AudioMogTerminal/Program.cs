b8de4ac343fda0e8ca696341564af1d2459f7ba4
Yoraiz0r <yoraimer@hotmail.com>
2021-06-05 16:10:41 +0300

    Updated version number for release

--- AudioMogTerminal/Program.cs
+++ AudioMogTerminal/Program.cs
@@ -13,8 +13,8 @@ namespace AudioMog.Terminal
 {
 	internal class Program
 	{
-		public const string AssemblyVersion = "2021.06.06";
-		public const string AssemblyFileVersion = "2021.06.06";
+		public const string AssemblyVersion = "2021.06.05.02";
+		public const string AssemblyFileVersion = "2021.06.05.02";
 		
 		private static ServiceProvider _serviceProvider;
 		public static ConsoleApplicationLogger Logger;
