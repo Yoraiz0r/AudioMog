102ffc1c5a6ca532bc1f7e19d001079ff5106926
Yoraiz0r <yoraimer@hotmail.com>
2025-02-16 08:28:01 +0200

    In cases where file contains both 'sabf' and 'mabf' strings, process the one that is found first.

--- AudioMogApplication/FileParser.cs
+++ AudioMogApplication/FileParser.cs
@@ -15,11 +15,35 @@ namespace AudioMog.Application
 
 		public AAudioBinaryFile Parse(byte[] fileBytes)
 		{
-			if (TryReading(fileBytes, MabfString, Settings.MusicFileFileVersion, out MusicAudioBinaryFile mabf))
-				return mabf;
 
-			if (TryReading(fileBytes, SabfString, Settings.SoundFileFileVersion, out SoundAudioBinaryFile sabf))
-				return sabf;
+			var hasSabf = TryGetInternalFilePosition<SoundAudioBinaryFile>(fileBytes, SabfString, out var sabfStart);
+			var hasMabf = TryGetInternalFilePosition<MusicAudioBinaryFile>(fileBytes, MabfString, out var mabfStart);
+
+			var sabfFirst = false;
+			var mabfFirst = false;
+
+			if (hasSabf && hasMabf)
+			{
+				sabfFirst = sabfStart < mabfStart;
+				mabfFirst = !sabfFirst;
+			}
+			else
+			{
+				sabfFirst = hasSabf;
+				mabfFirst = hasMabf;
+			}
+
+			if (mabfFirst && TryReading(fileBytes, MabfString, Settings.MusicFileFileVersion, out MusicAudioBinaryFile mab))
+				return mab;
+
+			if (sabfFirst && TryReading(fileBytes, SabfString, Settings.SoundFileFileVersion, out SoundAudioBinaryFile sab))
+				return sab;
+
+			if (TryReading(fileBytes, MabfString, Settings.MusicFileFileVersion, out mab))
+				return mab;
+
+			if (TryReading(fileBytes, SabfString, Settings.SoundFileFileVersion, out sab))
+				return sab;
 			
 			throw new FileDoesNotContainAudioBinaryException();
 		}
@@ -28,18 +52,26 @@ namespace AudioMog.Application
 		{
 			file = null;
 			
-			var internalFileStartPosition = fileBytes.FindSubArray(arrayOfBytesMagic);
-			if (internalFileStartPosition < 0)
+			if (!TryGetInternalFilePosition<TAudioBinaryFileType>(fileBytes, arrayOfBytesMagic, out var internalFileStartPosition))
 				return false;
 
 			if (!FindExpectedVersion(fileBytes, internalFileStartPosition + arrayOfBytesMagic.Length,
-				expectedVersion, out var failureReason))
+					expectedVersion, out var failureReason))
 				Logger.Warn(failureReason);
 			
 			file = new TAudioBinaryFileType();
 			file.Read(fileBytes, internalFileStartPosition);
 			return true;
 
+		}
+
+		private static bool TryGetInternalFilePosition<TAudioBinaryFileType>(byte[] fileBytes, byte[] arrayOfBytesMagic,
+			out long internalFileStartPosition) where TAudioBinaryFileType : AAudioBinaryFile, new()
+		{
+			internalFileStartPosition = fileBytes.FindSubArray(arrayOfBytesMagic);
+			if (internalFileStartPosition < 0)
+				return false;
+			return true;
 		}
 
 		private bool FindExpectedVersion(byte[] fileBytes, long position, AudioBinaryFileVersion version, out string failureReason)
